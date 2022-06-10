0e73430eb40a4da75df9ea198ba45d48b82cbcea
tomedwan <ilyauiv2000@gmail.com>
2022-06-10 20:44:29 +0700

    1

---
+++ 931903.yurkevich.ilya.lab2.2/Backend2/Controllers/Calc.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend2.Models;
+namespace Backend2.Controllers
+{
+    public class Calc : Controller
+    {
+        public IActionResult Manual()
+        {
+            if (Request.Method == "POST")
+            {
+                Actions actions = new Actions();
+                actions.val1 = Request.Form["val1"];
+                actions.val2 = Request.Form["val2"];
+                actions.oper = Request.Form["oper"];
+                int x;
+                if (int.TryParse(actions.val1, out x) && int.TryParse(actions.val2, out x))
+                    ViewBag.Result = actions.getResult();
+                else ViewBag.Result = "Nulls Components Exists";
+                return View("Result");
+            }
+            else
+                return View();
+        }
+        [HttpGet]
+        public IActionResult ManualWithSeparateHandlers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ManualWithSeparateHandlers(string str)
+        {
+            Actions actions = new Actions();
+            actions.val1 = Request.Form["val1"];
+            actions.val2 = Request.Form["val2"];
+            actions.oper = Request.Form["oper"];
+            int x;
+            if (int.TryParse(actions.val1, out x) && int.TryParse(actions.val2, out x))
+                ViewBag.Result = actions.getResult();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInParameters(string val1, string val2, string oper)
+        {
+            Actions  actions= new Actions();
+            actions.val1 = val1;
+            actions.val2 = val2;
+            actions.oper = oper;
+            int x;
+            if (int.TryParse(actions.val1, out x) && int.TryParse(actions.val2, out x))
+                ViewBag.Result = actions.getResultModel();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpGet]
+        public IActionResult ModelBindingInParameters()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInSeparateModel(Actions actions)
+        {
+            int x;
+            if (int.TryParse(actions.val1, out x) && int.TryParse(actions.val2, out x))
+                ViewBag.Result = actions.getResultModel();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpGet]
+        public IActionResult ModelBindingInSeparateModel()
+        {
+            return View();
+        }
+    }
+}
---
+++ 931903.yurkevich.ilya.lab2.2/Backend2/Models/Actions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend2.Models
+{
+    public class Actions
+    {
+        public string val1 { get; set; }
+        public string val2 { get; set; }
+        public string oper { get; set; }
+
+        public string getResult()
+        {
+            int a = Int32.Parse(val1);
+            int b = Int32.Parse(val2);
+            switch ((int)Convert.ToChar(oper))
+            {
+                case 42: return (val1) + " * " + (val2) + " = " + (a * b);
+                case 43: return (val1) + " + " + (val2) + " = " + (a + b);
+                case 45: return (val1) + " - " + (val2) + " = " + (a - b);
+                case 47:
+                    try
+                    {
+                        return (val1) + " / " + (val2) + " = " + (a / b);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return "Exception: Division by zero.";
+                    }
+
+            }
+            return "b";
+        }
+        public string getResultModel()
+        {
+            int a = Int32.Parse(val1);
+            int b = Int32.Parse(val2);
+            int c;
+            switch ((int)Convert.ToChar(oper))
+            {
+                case 42:
+                    c = a * b;
+                    return c.ToString();
+                case 43:
+                    c = a + b;
+                    return c.ToString();
+                case 45:
+                    c = a - b;
+                    return c.ToString();
+                case 47:
+                    try
+                    {
+                        c = a / b;
+                        return c.ToString();
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return "Exception: Division by zero.";
+                    }
+            }
+            return "b";
+        }
+    }
+}
